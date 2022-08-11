34f428f22e3024baff6ddc0e7d491bf1854d708d
yana_khaustova <yana.haus@gmail.com>
2022-08-11 13:36:58 +0300

    добавлены комменты к коду

---
+++ test_add_company.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+
+class TestAddCompany(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+    def test_add_company(self):
+        wd = self.wd
+        # open home page
+        wd.get("http://localhost/addressbook/")
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        # init company creation
+        wd.find_element_by_link_text("add new").click()
+        # fill company form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Yana")
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("H")
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("yana_haus")
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("Title")
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("Company22")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Spb")
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("8990")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("3434")
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys("3434")
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys("3443")
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("yana.haus@mail.ru")
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text("7")
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("June")
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1987")
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text("1")
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text("March")
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys("2000")
+        # submit company creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # return home page
+        wd.find_element_by_link_text("home").click()
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
