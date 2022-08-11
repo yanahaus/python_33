565d11e2c242cbe1f147fb19c7c9310485619ca7
yana_khaustova <yana.haus@gmail.com>
2022-08-11 22:36:24 +0300

    создание класса Group_сompany

--- group.py
+++ group.py
@@ -4,3 +4,25 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+class Group_сompany:
+
+    def __init__(self, name, lastname, nickname, title, company, adress, home, mobile, work_phone, fax,
+                       email, bday, bmonth, byear, aday, amonth, ayear):
+        self.name = name
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.adress = adress
+        self.home = home
+        self.mobile = mobile
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
--- test_add_company.py
+++ test_add_company.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group_сompany
 
 
 class TestAddCompany(unittest.TestCase):
@@ -14,9 +15,25 @@ class TestAddCompany(unittest.TestCase):
     def test_add_company(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_company_page(wd)
-        self.create_company(wd)
+        self.create_company(wd, Group_сompany(name="Yana", lastname="H", nickname="yana_haus", title="Title", company="Company22",
+                            adress="Spb", home="8990", mobile="3434", work_phone="3434", fax="3443",
+                            email="yana.haus@mail.ru", bday="7", bmonth="June", byear="1987", aday="1", amonth="March",
+                            ayear="2000"))
+        self.return_home_page(wd)
+        self.logout(wd)
+
+
+    def test_add_empty_company(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_company_page(wd)
+        self.create_company(wd, Group_сompany(name="", lastname="", nickname="", title="", company="",
+                            adress="", home="", mobile="", work_phone="", fax="",
+                            email="", bday="", bmonth="", byear="", aday="", amonth="",
+                            ayear=""))
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -26,63 +43,63 @@ class TestAddCompany(unittest.TestCase):
     def return_home_page(self, wd):
         wd.find_element_by_link_text("home").click()
 
-    def create_company(self, wd):
+    def create_company(self, wd, group):
         # fill company form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Yana")
+        wd.find_element_by_name("firstname").send_keys(group.name)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("H")
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("yana_haus")
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Title")
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Company22")
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Spb")
+        wd.find_element_by_name("address").send_keys(group.adress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("8990")
+        wd.find_element_by_name("home").send_keys(group.home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("3434")
+        wd.find_element_by_name("mobile").send_keys(group.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("3434")
+        wd.find_element_by_name("work").send_keys(group.work_phone)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("3443")
+        wd.find_element_by_name("fax").send_keys(group.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("yana.haus@mail.ru")
+        wd.find_element_by_name("email").send_keys(group.email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("7")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(group.bday)
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("June")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(group.bmonth)
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1987")
+        wd.find_element_by_name("byear").send_keys(group.byear)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("1")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(group.aday)
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("March")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(group.amonth)
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2000")
+        wd.find_element_by_name("ayear").send_keys(group.ayear)
         # submit company creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def open_company_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
