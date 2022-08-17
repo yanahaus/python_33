a1d1b52e006a47ca364d6816c47598dadcecf632
Yana Khaustova <HaustovaY@quantumart.ru>
2022-08-17 12:53:19 +0300

    выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,19 +2,19 @@
 
 import pytest
 from model.class_for_test import Contact
-from fixture.application import ApplicationContact
+from fixture.application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationContact()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(name="Yana", lastname="H", nickname="yana_haus", title="Title", company="Company22",
+    app.contact.create_contact(Contact(name="Yana", lastname="H", nickname="yana_haus", title="Title", company="Company22",
                                adress="Spb", home="8990", mobile="3434", work_phone="3434", fax="3443",
                                email="yana.haus@mail.ru", bday="7", bmonth="June", byear="1987", aday="1",
                                amonth="March", ayear="2000"))
@@ -23,7 +23,7 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(name="", lastname="", nickname="", title="", company="", adress="", home="", mobile="",
+    app.contact.create_contact(Contact(name="", lastname="", nickname="", title="", company="", adress="", home="", mobile="",
                                work_phone="", fax="", email="", bday="", bmonth="", byear="", aday="", amonth="",
                                ayear=""))
     app.session.logout()
