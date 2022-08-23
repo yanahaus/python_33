6482634ac5a607ce0e109626971ed2f68542902b
Yana Khaustova <HaustovaY@quantumart.ru>
2022-08-23 14:31:34 +0300

    fix test group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,7 +5,7 @@ from model.class_for_test import Group
 
 def test_edit_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit(Group(name="name1", header="one1", footer="two1"))
+    app.group.edit_first(Group(name="nnnn", header="tttt", footer="sss"))
     app.session.logout()
 
 
