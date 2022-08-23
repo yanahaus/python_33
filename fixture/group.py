0949cf3777e49b3b7dfce959f738f34d0497924b
Yana Khaustova <HaustovaY@quantumart.ru>
2022-08-23 21:26:01 +0300

    модифицирован est_edit_group для возожности изменения только одного параметра

--- fixture/group.py
+++ fixture/group.py
@@ -23,17 +23,21 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         # edit contact form
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
+        # edit contact form
         wd.find_element_by_name("edit").click()
         self.fill_group_form(group)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -44,12 +48,14 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_fild_value("group_name", group.name)
+        self.change_fild_value("group_header", group.header)
+        self.change_fild_value("group_footer", group.footer)
+
+
+    def change_fild_value(self, selector, value):
+        wd = self.app.wd
+        if value is not None:
+            wd.find_element_by_name(selector).click()
+            wd.find_element_by_name(selector).clear()
+            wd.find_element_by_name(selector).send_keys(value)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,7 +5,7 @@ from model.class_for_test import Group
 
 def test_edit_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first(Group(name="nnnn", header="tttt", footer="sss"))
+    app.group.edit_first(Group(name="new2"))
     app.session.logout()
 
 
