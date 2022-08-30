1261cb1841ee9f2f556f402aaea36e44c9ed2036
Yana Khaustova <HaustovaY@quantumart.ru>
2022-08-30 22:16:40 +0300

    добавлены проверки для тестов edit_contacts и del_contacts

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -6,5 +6,10 @@ def test_delete_first_contact(app):
                                    company="Company22", adress="Spb", home="8990", mobile="3434", work_phone="3434",
                                    fax="3443", email="yana.haus@mail.ru", bday="7", bmonth="June", byear="1987",
                                    aday="1", amonth="March", ayear="2000"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
+
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -9,8 +9,15 @@ def test_edit_first_contact(app):
                                    company="Company22", adress="Spb", home="8990", mobile="3434", work_phone="3434",
                                    fax="3443", email="yana.haus@mail.ru", bday="7", bmonth="June", byear="1987",
                                    aday="1", amonth="March", ayear="2000"))
-    app.contact.edit_first(Contact(name="Кирилл", lastname="245", nickname="yana_haus1", title="Title1",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(name="Кирилл", lastname="245", nickname="yana_haus1", title="Title1",
                                            company="Company221", adress="Spb1", home="89901", mobile="34341",
                                            work_phone="34341", fax="34431", email="yana.haus1@mail.ru", bday="6",
-                                           bmonth="March", byear="1988", aday="12", amonth="June", ayear="2001"))
+                                           bmonth="March", byear="1988", aday="12", amonth="June", ayear="2001")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first(contact)
+    new_contacts = app.contact.get_contact_list()
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
