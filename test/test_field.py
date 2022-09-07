e6f27dc85242ad5592e13cc73eda0c2739cd427f
Yana Khaustova <HaustovaY@quantumart.ru>
2022-09-07 12:56:13 +0300

    объединены все тесты по полям в один, добавлен clear в field_from_home_page.all_emails_from_home_page

--- test/test_field.py
+++ test/test_field.py
@@ -1,9 +1,15 @@
 import re
 
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+def test_field_on_home_page(app):
+    field_from_home_page = app.contact.get_contact_list()[0]
+    field_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert field_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(field_from_edit_page)
+    assert field_from_home_page.firstname == clear(field_from_home_page.firstname)
+    assert field_from_edit_page.lastname == clear(field_from_edit_page.lastname)
+    assert field_from_home_page.address == clear(field_from_edit_page.address)
+    assert clear(field_from_home_page.all_emails_from_home_page) == merge_emails_like_on_home_page(field_from_edit_page)
+
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
@@ -13,31 +19,17 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
-def test_name_on_home_page(app):
-    name_from_home_page = app.contact.get_contact_list()[0]
-    name_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert name_from_home_page.firstname == clear(name_from_edit_page.firstname)
-    assert name_from_home_page.lastname == clear(name_from_edit_page.lastname)
-
-def test_adress_on_home_page(app):
-    adress_from_home_page = app.contact.get_contact_list()[0]
-    adress_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert adress_from_home_page.address == clear(adress_from_edit_page.address)
-
-
-def test_email_on_home_page(app):
-    email_from_home_page = app.contact.get_contact_list()[0]
-    email_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert email_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(email_from_edit_page)
 
 def clear(s):
     return re.sub("[- ()]", "", s)
+
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != '',
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.homephone, contact.workphone, contact.mobilephone, contact.phone2]))))
+
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != '',
