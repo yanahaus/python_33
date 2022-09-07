62a9c314d32841f6aa912befdc70bb08502e7071
Yana Khaustova <HaustovaY@quantumart.ru>
2022-09-07 18:00:58 +0300

    bag_clear

--- model/class_for_test.py
+++ model/class_for_test.py
@@ -2,17 +2,17 @@ from sys import maxsize
 
 class Group:
 
-    def __init__(self, name=None, header=None, footer=None, id=None):
+    def __init__(self, id=None, name=None, header=None, footer=None):
+        self.id = id
         self.name = name
         self.header = header
         self.footer = footer
-        self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s: %s; %s; %s" % (self.id, self.name, self.header, self.footer)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
 
     def id_or_max(self):
         if self.id:
