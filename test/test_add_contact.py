9b809fc78e7be175d118210987f3c6947d55fc4d
Yana Khaustova <HaustovaY@quantumart.ru>
2022-09-08 12:46:40 +0300

    Параметризованы тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,37 @@
 # -*- coding: utf-8 -*-
 
-
 from model.class_for_test import Contact
+import pytest
+import random
+import string
+import re
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    stroka = prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    stroka = re.sub('\s+', ' ', stroka).strip()
+    return stroka
 
 
-def test_add_contact(app):
+def random_number(prefix):
+    symbols = string.digits
+    number = prefix + "".join([random.choice(symbols) for i in range(10)])
+    return number
+
+
+testdata = [Contact(firstname="", lastname="", nickname="", title="", company="", address="", homephone="",
+                    mobilephone="", workphone="", fax="", email="", bday="", bmonth="", byear="", aday="", amonth="",
+                    ayear="", phone2="")] + [Contact(firstname=random_string("firstname", 10),
+                    lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                    title=random_string("title", 10), company=random_string("company", 10),
+                    address=random_string("adress", 10), homephone=random_number("+79"), mobilephone=random_number("+79"),
+                    workphone=random_number("+79"), fax="3441", email="yana.haus@mail.ru", bday="7", bmonth="June",
+                    byear="1987", aday="1", amonth="March", ayear="2000", phone2=random_number("+79")) for i in range(3)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", lastname="Petrov", nickname="yana_haus", title="Title", company="Company22",
-                      address="Spb", homephone="8990", mobilephone="3434", workphone="123", fax="3441",
-                      email="yana.haus@mail.ru", bday="7", bmonth="June", byear="1987", aday="1",
-                      amonth="March", ayear="2000", phone2="Number")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,12 +8,21 @@ import re
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    stroka = prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    stroka = re.sub('\s+', ' ', stroka).strip()
+    return stroka
 
 
 testdata = [Group(name="", header="", footer="")] + [Group(name=random_string("group_name", 10), header=random_string("header_group", 20),
-                  footer=random_string("footer_group", 20)) for i in range(5)
+                  footer=random_string("footer_group", 20)) for i in range(5)]
+
+"""Пример реализации комбинации значений
+testdata = [Group(name=name, header=header, footer=footer)
+            for name in ["", random_string("group_name", 10)]
+            for header in ["", random_string("header_group", 20)]
+            for footer in ["", random_string("footer_group", 10)]
             ]
+            ]"""
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
@@ -23,9 +32,9 @@ def test_add_group(app, group):
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
-    assert sorted(clear(old_groups), key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def clear(s):
-    return re.sub("[  ]", " ", s)
+
+
 
