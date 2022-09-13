9e0e3533a1cc4afe2dbdc57a901a6d0b76dd676a
Yana Khaustova <HaustovaY@quantumart.ru>
2022-09-13 21:20:23 +0300

    метод clear разбит на 2: для email и для телефонов

--- test/test_field.py
+++ test/test_field.py
@@ -8,7 +8,7 @@ def test_field_on_home_page(app):
     assert clear(field_from_home_page.firstname) == clear(field_from_home_page.firstname)
     assert clear(field_from_edit_page.lastname) == clear(field_from_edit_page.lastname)
     assert clear(field_from_home_page.address) == clear(field_from_edit_page.address)
-    assert clear(field_from_home_page.all_emails_from_home_page) == merge_emails_like_on_home_page(field_from_edit_page)
+    assert clear_email(field_from_home_page.all_emails_from_home_page) == merge_emails_like_on_home_page(field_from_edit_page)
 
 
 """def test_phones_on_contact_view_page(app):
@@ -22,8 +22,10 @@ def test_field_on_home_page(app):
 
 
 def clear(s):
-    return re.sub("[\s ()]", "", s)
+    return re.sub("[\s ()-]", "", s)
 
+def clear_email(s):
+    return re.sub("\s", "", s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != '',
@@ -34,6 +36,6 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != '',
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_email(x),
                                 filter(lambda x: x is not None,
                                        [contact.email, contact.email2, contact.email3]))))
