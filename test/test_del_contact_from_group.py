7062e0c78a4f38785abec35e6d151460046160b7
Yana Khaustova <HaustovaY@quantumart.ru>
2022-09-30 10:44:22 +0300

    Реализованы тесты для добавления контакта в группу и удаления контакта из группы_4.0

--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -11,11 +11,13 @@ def test_del_contact_in_group(app, db):
     contacts = db.get_contact_list()
     if len(groups) == 0:
         app.group.create(Group(name="name", header="one", footer="two"))
+        groups = db.get_group_list()
     if len(contacts) == 0:
         app.contact.create(Contact(firstname="Permanent", lastname="H2", nickname="yana_haus", title="Title",
                                    company="Company22", address="Spb", homephone="8990", mobilephone="3434",
                                    workphone="3434", fax="3443", email="yana.haus@mail.ru", bday="7", bmonth="June",
                                    byear="1987", aday="1", amonth="March", ayear="2000", phone2="233333"))
+        contacts = db.get_contact_list()
 
     random_group = random.choice(groups)
     old_contacts_in_group = orm.get_contacts_in_group(random_group)
